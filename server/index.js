73d19e0e84de4980093002750d40bc85338aa1e9
Antonio B <39917231+Abourass@users.noreply.github.com>
2020-04-16 13:28:16 -0600

    :art: Upgrading dependencies and fixing linting errors

--- server/index.js
+++ server/index.js
@@ -1,12 +1,12 @@
-const Koa = require('koa');
-const koaBody = require('koa-body');
-const logger = require('koa-logger');
-const mongoose = require('mongoose');
-const helmet = require('koa-helmet');
-const ip = require('ip');
-const cors = require('@koa/cors');
-const routing = require('./routes/routeHandler.js');
-const {port, mongo} = require('./utils/port.js');
+const Koa = require('koa'),
+  koaBody = require('koa-body'),
+  logger = require('koa-logger'),
+  mongoose = require('mongoose'),
+  helmet = require('koa-helmet'),
+  ip = require('ip'),
+  cors = require('@koa/cors'),
+  routing = require('./routes/routeHandler.js'),
+  {port, mongo} = require('./utils/port.js');
 mongoose.connect(mongo.uri, mongo.config).catch(err => console.error); // Create Database Connection
 
 const app = new Koa(); // Create Koa Server
@@ -18,9 +18,9 @@ app.use(cors());
 
 routing(app); // Start Routes
 
-const server = require('http').createServer(app.callback());
-const io = require('socket.io')(server);
-server.listen(port, () => { console.log(`Server is now running on http://${ip.address()}:${port}`);}); // Start the server
+const server = require('http').createServer(app.callback()),
+  io = require('socket.io')(server);
+server.listen(port, () => { console.log(`Server is now running on http://${ip.address()}:${port}`); }); // Start the server
 let usersOnline = [];
 io.on('connection', (socket) => {
   socket.emit('OnlineService', {AuthCheckIsOnline: true});
@@ -28,14 +28,14 @@ io.on('connection', (socket) => {
     console.log(data);
     if (usersOnline.filter(obj => obj.user === data.user).length > 0){
       usersOnline = usersOnline.filter(obj => obj.user !== data.user);
-      usersOnline.push(data)
+      usersOnline.push(data);
     } else {
       usersOnline.push(data);
     }
     socket.emit('Online', usersOnline);
     socket.on('disconnect', () => {
       console.log(`${data.user} disconnected`);
-      usersOnline = usersOnline.filter(obj => obj.user !== data.user)
+      usersOnline = usersOnline.filter(obj => obj.user !== data.user);
     });
   });
 });
