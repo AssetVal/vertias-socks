03229c453d2a66eff57e84e6b73693afd93687e1
abourass <abourassa@assetval.com>
2020-11-28 16:18:23 -0700

    :arrow-up: Upgrading npm versions

--- server/index.js
+++ server/index.js
@@ -1,12 +1,12 @@
-const Koa = require('koa'),
-  koaBody = require('koa-body'),
-  logger = require('koa-logger'),
-  mongoose = require('mongoose'),
-  helmet = require('koa-helmet'),
-  ip = require('ip'),
-  cors = require('@koa/cors'),
-  routing = require('./routes/routeHandler.js'),
-  {port, mongo} = require('./utils/port.js');
+const Koa = require('koa');
+const koaBody = require('koa-body');
+const logger = require('koa-logger');
+const mongoose = require('mongoose');
+const helmet = require('koa-helmet');
+const ip = require('ip');
+const cors = require('@koa/cors');
+
+const {port, mongo} = require('./utils/port.js');
 mongoose.connect(mongo.uri, mongo.config).catch(err => console.error); // Create Database Connection
 
 const app = new Koa(); // Create Koa Server
@@ -25,10 +25,12 @@ app.use(helmet.referrerPolicy());
 app.use(helmet.xssFilter());
 app.use(cors());
 
-routing(app); // Start Routes
-
 const server = require('http').createServer(app.callback());
 const io = require('socket.io')(server);
+
+const routing = require('./routes/routeHandler.js');
+routing(app); // Start Routes
+
 server.listen(port, () => { console.log(`Server is now running on http://${ip.address()}:${port}`); }); // Start the server
 let usersOnline = [];
 io.on('connection', (socket) => {
