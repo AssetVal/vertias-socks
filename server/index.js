d07bc87649565f8d8cb5b07b349d3d62067fcf40
abourass <abourassa@assetval.com>
2020-11-29 11:57:56 -0700

    :lock: Enabled CORS in socket.io

--- server/index.js
+++ server/index.js
@@ -10,15 +10,17 @@ const app = new Koa(); // Create Koa Server
 
 app.use(logger());
 app.use(koaBody());
-app.use(cors({
-  origin: '*',
-  method: 'OPTIONS',
-}));
+app.use(cors());
 app.use(helmet());
 
 const server = require('http').createServer(app.callback());
 const {Server} = require('socket.io');
-const io = new Server(server);
+const io = new Server(server, {
+  cors: {
+    origin: ['https://www.assetval.club/', 'http://192.168.56.1:5000/'],
+    methods: ['GET', 'POST'],
+  },
+});
 
 const routing = require('./routes/routeHandler.js');
 const {port, mongo} = require('./utils/port.js');
