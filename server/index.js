cb966f8ddfe872bf93d789f44c68a4e031f70260
abourass <abourassa@assetval.com>
2020-11-28 18:28:18 -0700

    :lock: Fixing cors header

--- server/index.js
+++ server/index.js
@@ -17,7 +17,7 @@ app.use(helmet.hidePoweredBy());
 app.use(helmet.hsts());
 app.use(helmet.ieNoOpen());
 app.use(helmet.noSniff());
-app.use(helmet.permittedCrossDomainPolicies());
+// app.use(helmet.permittedCrossDomainPolicies());
 app.use(helmet.referrerPolicy());
 app.use(helmet.xssFilter());
 app.use(cors());
