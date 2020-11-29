4e7e25bcf013ebb8d3b312b88faf52292dd65a93
abourass <abourassa@assetval.com>
2020-11-28 18:50:38 -0700

    :lock: Fixing cors header

--- server/index.js
+++ server/index.js
@@ -13,6 +13,18 @@ app.use(koaBody());
 app.use(cors({
   origin: '*',
 }));
+app.use(helmet.dnsPrefetchControl());
+app.use(helmet.expectCt());
+app.use(helmet.frameguard());
+app.use(helmet.hidePoweredBy());
+app.use(helmet.hsts());
+app.use(helmet.ieNoOpen());
+app.use(helmet.noSniff());
+app.use(helmet.permittedCrossDomainPolicies());
+app.use(helmet.referrerPolicy({
+  policy: ['origin', 'unsafe-url'],
+}));
+app.use(helmet.xssFilter());
 
 const server = require('http').createServer(app.callback());
 const {Server} = require('socket.io');
