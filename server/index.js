794e2204b9de9130eae3877bc4c170ec659f141f
Abourass <abourassa@assetval.com>
2020-11-30 06:57:42 -0700

    :art: Forgot to remove a backslash

--- server/index.js
+++ server/index.js
@@ -20,7 +20,7 @@ const io = new Server(server, {
     origin: [
       'https://www.assetval.club', // Staging server
       'http://192.168.56.1:5000',  // Antonio Home
-      'http://192.168.245.108:5000/' // Antonio Work
+      'http://192.168.245.108:5000'// Antonio Work
     ],
     methods: ['GET', 'POST'],
     allowedHeaders: ['assetcat-the-wondercat'],
